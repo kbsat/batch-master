605b548de926a470221aa59dcfb1cb27f5e24d4e
kbs <kbs@woowahan.com>
2023-01-10 01:38:27 +0900

    spring batch 학습

---
+++ src/main/java/com/example/batchmaster/BatchMasterApplication.java
@@ -0,0 +1,14 @@
+package com.example.batchmaster;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing // 배치 기능 활성화
+@SpringBootApplication
+public class BatchMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchMasterApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/batchmaster/job/SimpleJobConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.batchmaster.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob") // simpleJob 이라는 이름으로 잡 생성
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep1") // simpleStep1 이라는 이름으로 스텝 생성
+                .tasklet(((contribution, chunkContext) -> { // tasklet은 step 안에서 단일로 수행될 커스텀한 기능 선언
+                    log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+}
